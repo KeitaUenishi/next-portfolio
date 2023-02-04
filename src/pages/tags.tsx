217208e3c25cbbdc9926f1d604977bee6b761425
KeitaUenishi <keita.uenishi@gmail.com>
2023-02-04 15:54:57 +0900

    install at react-daisyui

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Button } from 'react-daisyui'
 
 import { Layout } from 'components/layouts/Layout'
 
 export default function Tags() {
   return (
     <Layout>
-      <div>Tagいーーーっぱい</div>
+      <Button color="primary">Tagいーーーっぱい</Button>
     </Layout>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+  ],
   theme: {
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#808080',
+          secondary: '#D926AA',
+          accent: '#1FB2A5',
+          neutral: '#191D24',
+          'base-100': '#2A303C',
+          info: '#3ABFF8',
+          success: '#36D399',
+          warning: '#FBBD23',
+          error: '#F87272',
+        },
+      },
+    ],
+  },
 }
